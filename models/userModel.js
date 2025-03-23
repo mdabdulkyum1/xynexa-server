822474efbf5db3a1b60c44777be354965a827928
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-03-24 00:59:33 +0600

    fix register issue

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
-// abdul: Define user schema
-
+// Define user schema
 const userSchema = new mongoose.Schema({
   clerkId: { type: String, required: true, unique: true }, // Clerk user ID
   firstName: { type: String, required: true },
   lastName: { type: String },
   email: { type: String, required: true, unique: true },
   imageUrl: { type: String },
-  role: { type: String, required: true }, // Role (Admin, Member, Viewer, etc.)
-},
-{ timestamps: true, versionKey: false }
-);
+  role: { type: String, required: true, default: "member" } // Default role set to "member"
+}, { timestamps: true, versionKey: false });
 
+// Create Mongoose model
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
