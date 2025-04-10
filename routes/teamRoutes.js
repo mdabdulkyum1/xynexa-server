beab6896658b1989f52e292af2812d8fe65a16dc
harun708280 <harun708280@gmail.com>
2025-04-11 00:09:24 +0600

    update Api documentTation

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteTeam,
   addMemberToTeam,
   getUserTeams,
+  getUserTeamsByEmail, 
 } from "../controllers/teamController.js";
 
 const router = express.Router();
@@ -14,6 +15,7 @@ router.post("/create", createTeam); // Create team
 router.get("/:id", getTeam); // Get team by ID
 
 router.get("/user/teams/:userId", getUserTeams); // Get user's teams (ID from params)
+router.get("/user/teams/email/:userEmail", getUserTeamsByEmail); // Get user's teams (email from params) // new line added
 router.put("/:id", updateTeam); // Update team
 router.delete("/:id", deleteTeam); // Delete team
 router.post("/addMember", addMemberToTeam); // Add member to team
