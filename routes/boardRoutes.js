51736231592ec11ce0b10f1429c13fa62920ee45
harun708280 <harun708280@gmail.com>
2025-04-12 18:06:27 +0600

    task board  crud api and add member and atcahment

--- routes/boardRoutes.js
+++ routes/boardRoutes.js
@@ -1,10 +1,43 @@
-import express from "express";
-import { createTask, getAllTasksBy } from "../controllers/boardController.js";
+import express from 'express';
+import {
+    createBoard,
+    getBoardById,
+    updateBoard,
+    deleteBoard,
+    addMemberToBoard,
+    addCommentToBoard,
+    updateBoardStatus,
+    addAttachmentToBoard,
+    getBoardsByTeamId,
+} from '../controllers/boardController.js';
 
 const router = express.Router();
 
-router.post("/create", createTask);
+// Create a new board
+router.post('/create', createBoard);
 
-router.get("/getAllTask", getAllTasksBy);
+// Get a board by ID
+router.get('/:id', getBoardById);
+
+// Update a board
+router.put('/:id', updateBoard);
+
+// Delete a board
+router.delete('/:id', deleteBoard);
+
+// Add a member to a board
+router.post('/members', addMemberToBoard);
+
+// Add a comment to a board
+router.post('/comments', addCommentToBoard);
+
+// Update the status of a board
+router.put('/status', updateBoardStatus);
+
+// Add an attachment to a board
+router.post('/attachments', addAttachmentToBoard);
+
+// Get boards by team ID
+router.get('/team/:teamId', getBoardsByTeamId);
 
 export default router;
